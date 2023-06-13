74930c3ab0b9ed496b011a7230c036763e302a20
Rohit <rohitsaste45@gmail.com>
2023-06-13 20:35:47 +0530

    msg

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 function addRcord(){
-    let uri =`http://localhost:4000/addRecord?message=rohiiiiiit` ;
+    let inputref = document.querySelector("#messageId");
+    let message = inputref.value;
+    let uri =`http://localhost:4000/addRecord?message=${message}`;
     fetch(uri);
+
+    inputref.value="";
+}
+
+async function readRecord(){
+    let uri =`http://localhost:4000/findAllMessage`;
+    let res = await fetch(uri);
+    
+    let list = await res.json();
+
+    console.log(list);
+
+    for(let item of list ){
+        let pref = document.querySelector("#parent");
+        let evalue = pref.innerHTML;
+        let nvalue =`<h>${item.message}</h1>`;
+        pref.innerHTML= nvalue +evalue;
+        
+    }
+ 
 }
